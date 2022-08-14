847733eb42e1f4cc83f0f6b1060210b20d23fceb
陋室居士 <108336535+lala123963@users.noreply.github.com>
2022-08-14 23:19:17 +0800

    Update and rename 3顺丰速递+.js to 3顺丰速递.js

--- 3顺丰速递.js
+++ 3顺丰速递.js
@@ -27,7 +27,7 @@
 cron: 12 12 12 * * *
  */
 
-const $ = new Env("顺丰速递+");
+const $ = new Env("顺丰速递");
 const notify = $.isNode() ? require("./sendNotify") : "";
 const Notify = 1 		//0为关闭通知,1为打开通知,默认为1
 const debug = 0 		//0为关闭调试,1为打开调试,默认为0
