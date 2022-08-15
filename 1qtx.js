1b417d51762a4a1d40cadc1ebc6defdabc913831
陋室居士 <108336535+lala123963@users.noreply.github.com>
2022-08-15 14:29:14 +0800

    Update 1qtx.js

--- 1qtx.js
+++ 1qtx.js
@@ -12,7 +12,7 @@
  cron: 10 10 12 * * *
  */
 
- const $ = Env('青碳行');
+ const $ = new Env('青碳行');
  const notify = $.isNode() ? require('./sendNotify') : '';
  const Notify = 1; //0为关闭通知，1为打开通知,默认为1
  const debug = 0; //0为关闭调试，1为打开调试,默认为0
