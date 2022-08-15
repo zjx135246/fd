8fd5ce699a36ddf41e3e482f86f809bcf2bb374f
陋室居士 <108336535+lala123963@users.noreply.github.com>
2022-08-15 13:10:20 +0800

    Add files via upload

---
+++ wkllq/soy_variable_data.js
@@ -0,0 +1,11 @@
+module.exports = {"code":200, 
+"variable_data":{ 
+"wkllq":[{ //悟空浏览器
+    "device_id":"3259427894338669", //链接上找device_id值
+    "x-tt-token":"004ba23c1add0cac5c8479c08bf52eb22003d002e25332ebf2ea9be50f7b2f54d3a8bd1a10e9b9cdd1c05eb3e34dd778a9d404534607e1a93324eee84db5fae1bb1c1dc84dcd4c0f25646e09c80dd14bece7ac752e1af89d6a4103c4592d715b65428-1.0.1", //请求头的值
+    "sessionid":"4ba23c1add0cac5c8479c08bf52eb220"} //请求头cookie上的值
+],
+"kyin":[//快音
+"d3d122eb05a828d3&eyJleHAiOjE2NTk4Nzc1NDksImtpZCI6NCwiYWxnIjoiSFMyNTYifQ.eyJ1aWQiOiIxMDk3NTM0MjMiLCJvcGVuaWQiOiJvWWJWTHhNMDJVeDBMTUtjc2NBMXBRNDR0RWZBIiwiZXhwIjoxNjU5ODc3NTQ5fQ.jOsYcQPn5mK6QvPHyW_P4cxrk_I18rCSLX1SJhLhFsI&eyJleHAiOjE2Njc2NTE3NDksImFsZyI6IkhTMjU2In0.eyJ1aWQiOiIxMDk3NTM0MjMiLCJvcGVuaWQiOiJvWWJWTHhNMDJVeDBMTUtjc2NBMXBRNDR0RWZBIiwiZXhwIjoxNjY3NjUxNzQ5fQ.Eg_R7wHFcCEJnKxkIoRWpM2ys8Muc_Jod81rqKbWXI8",
+"02D91DEE-A824-4F3C-BA97-8F9030D2B7FA&eyJleHAiOjE2NTk4Nzg0NzAsImtpZCI6MCwiYWxnIjoiSFMyNTYifQ.eyJ1aWQiOiIxMDgzOTY1NTYiLCJvcGVuaWQiOiJvWWJWTHhBQXdzMXhqd1RjUHRsblUwRkpudzdnIiwiZXhwIjoxNjU5ODc4NDcwfQ.bgBEDkE0VgFE7Kyv4ERuIr8i-lAFhpdHLlRUhleuCkg&eyJleHAiOjE2Njc2NTI2NzAsImFsZyI6IkhTMjU2In0.eyJ1aWQiOiIxMDgzOTY1NTYiLCJvcGVuaWQiOiJvWWJWTHhBQXdzMXhqd1RjUHRsblUwRkpudzdnIiwiZXhwIjoxNjY3NjUyNjcwfQ.gFzTu-c66MNGJIHULgOJksnBN84smWZUKahqdoaAMGQ"
+]}}
